7cf5ee199cba6e4320a630d532a01840cc420a0b
daniel bermùdez morales <daniel00ber@hotmail.com>
2023-05-15 23:17:34 -0500

    actualizacion cadena conexion

--- config/connections.js
+++ config/connections.js
@@ -7,8 +7,8 @@ const config = {
     password: process.env.PASSWORD_DB,
     database: process.env.DATABASE
 }
-const dbconector = mysql.createConnection('mysql://6onogqbabku2ljyp2cqr:pscale_pw_s7bFFtzxkUZ5h579a8cImlqpxnFi0aWkyPrqkhE7NmL@aws.connect.psdb.cloud/elcamello?ssl={"rejectUnauthorized":true}'
-);
+//const dbconector = mysql.createConnection('mysql://6onogqbabku2ljyp2cqr:pscale_pw_s7bFFtzxkUZ5h579a8cImlqpxnFi0aWkyPrqkhE7NmL@aws.connect.psdb.cloud/elcamello?ssl={"rejectUnauthorized":true}')
+const dbconector = mysql.createConnection('mysql://ags23594hu9cls3f4818:pscale_pw_lchXgyyTV1iw8WlVrh80GNAUvFQqTxPn1SsaNyoDxKb@aws.connect.psdb.cloud/elcamello?ssl={"rejectUnauthorized":true}');
 
 dbconector.escape = mysql.escape;
 
